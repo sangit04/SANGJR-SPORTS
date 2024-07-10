d047f2e65ccc2e098c7befbcde9bdbe59737d6d8
Phan Sang <pvsang04@gmail.com>
2024-07-10 17:19:19 +0700

    Update list.php

--- admin/danhmuc/list.php
+++ admin/danhmuc/list.php
@@ -13,7 +13,64 @@
                        echo '<option value="'.$id.'">'.$name.'</option>';
                     }
                     
+                    ?><div class="row2">
+         <div class="row2 font_title">
+          <h1>DANH SÁCH LOẠI HÀNG HÓA</h1>
+         </div>
+         <div class="row2 form_content ">
+          <form action="index.php?act=lissp" method="POST">
+            <!-- <input type="text" name="kyw">
+            <select name="iddm">
+                <option value="0" selected>Tất cả</option>
+                    <?php 
+                    // foreach ($listdanhmuc as $danhmuc) {
+                    //    extract($danhmuc);
+                    //    echo '<option value="'.$id.'">'.$name.'</option>';
+                    // }
+                    
                     ?>
+                </select>
+                <input type="submit" name="listok" value="GO"> -->
+                
+           <div class="row2 mb10 formds_loai">
+           <table>
+            <tr>
+                <th></th>
+                <th>MÃ DANH MỤC</th>
+                <th>TÊN DANH MỤC</th>
+                <th>THAO TÁC</th>
+            </tr>
+            <?php
+             foreach ($listdanhmuc as $dm) {
+                extract($dm);
+                $suadm="index.php?act=suadm&id=".$id;
+                $xoadm="index.php?act=xoadm&id=".$id;
+              
+                echo'
+                <tr>
+                <td><input type="checkbox" name="" id=""></td>
+                <td>'.$id.'</td>
+                <td>'.$name.'</td>
+              
+                
+                <td><a href="'.$suadm.'"><input type="button" value="Sửa"><a href="'.$xoadm.'"><input type="button" value="Xóa"></td>
+            </tr>';
+             }
+            ?>
+            
+            
+            
+           </table>
+           </div>
+           <div class="row mb10 ">
+         <input class="mr20" type="button" value="CHỌN TẤT CẢ">
+         <input  class="mr20" type="button" value="BỎ CHỌN TẤT CẢ">
+<a href="index.php?act=adddm"> <input  class="mr20" type="button" value="NHẬP THÊM"></a>
+           </div>
+          </form>
+         </div>
+        </div>
+
                 </select>
                 <input type="submit" name="listok" value="GO">
                 
